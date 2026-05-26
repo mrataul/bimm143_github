640b27d72e6b35bdba8800dd27b43b1068d23001
Mankeerat Rataul <mrataul@ucsd.edu>
2026-05-26 12:05:50 -0700

    Fixing everyting

---
+++ Week1&2stuff/Day4Project/Class04.R
@@ -0,0 +1,13 @@
+#' ---
+#' title: "Rataul - LabReport Day 4"
+#' author: "Mankeerat Rataul"
+#' date: "April 9th, 2026"
+#' ---
+
+
+
+x <- seq(from = 1, to =50, by = 0.1)
+plot(x, sin(x), type="l", col="purple", lwd=3)
+
+
+
